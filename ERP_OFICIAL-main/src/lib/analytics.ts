3a441d4bc2708ffe1f2c5c8686949eefde39f2dd
Alef Januario Arruda <93352058+AlefJanuarioArruda@users.noreply.github.com>
2025-10-01 09:58:26 -0300

    Delete ERP_OFICIAL-main directory

--- ERP_OFICIAL-main/src/lib/analytics.ts
+++
@@ -1,8 +0,0 @@
-export const trackEvent = (name: string, params?: Record<string, any>) => {
-  if (typeof window !== 'undefined' && window.dataLayer) {
-    window.dataLayer.push({
-      event: name,
-      ...params
-    });
-  }
-};
